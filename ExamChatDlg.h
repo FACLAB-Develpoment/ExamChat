e20306e4e16247f2efa4e779ad3bcfdebdd06995
swchief_office <cto@faclab.kr>
2021-09-29 18:39:39 +0900

    Created ExamChat Training Project.

---
+++ ExamChatDlg.h
@@ -0,0 +1,33 @@
+
+// ExamChatDlg.h : header file
+//
+
+#pragma once
+
+
+// CExamChatDlg dialog
+class CExamChatDlg : public CDialogEx
+{
+// Construction
+public:
+	CExamChatDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_EXAMCHAT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
